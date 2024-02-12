0a1c2316052ca5d9b952647dce5e9c48dfc59c82
AndyUA69 <103500963+AndyUA69@users.noreply.github.com>
2024-02-12 20:08:11 +0200

    Add files via upload

---
+++ components/Thinks/Thinks.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Thinks.css";
+
+const Thinks = (props) => {
+  return (
+    <>
+      {props.user.bio && (
+        <div
+          onClick={() => props.setActiveFriend(props.user.id)}
+          className="thinks"
+        >
+          <div className="img-wrp">
+            <img alt="profile" src={props.user.profilePic} />
+            <p>{props.user.bio}</p>
+          </div>
+          <h5>{props.user.username}</h5>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Thinks;

0a016244769e273acc15c915f8319a9db1bf5c51
AndyUA69 <103500963+AndyUA69@users.noreply.github.com>
2024-02-12 20:09:53 +0200

    Add files via upload

---
+++ components/UnSeen/UnSeen.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+import "./UnSeen.css";
+
+const UnSeen = (props) => {
+  const [friend, setFriend] = useState();
+
+  const getUser = () => {
+    props.users
+      .filter((f) => {
+        return f.id === props.user.id;
+      })
+      .map((f) => {
+        setFriend(f);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <>
+      {friend && (
+        <div className="unseen">
+          <div className="img-wrp">
+            <img alt="profile" src={friend.profilePic} />
+          </div>
+          <h3>{friend.username}</h3>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UnSeen;
